3b32f4038f0fe8895a1447c9570956c2a9f30f73
Grouvex <168425996+Grouvex@users.noreply.github.com>
2024-05-03 15:02:04 +0200

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -12,7 +12,7 @@ window.onload = function() {
         },
         {
             url: 'https://www.instagram.com/p/C6BPR5kooq0/',
-            title: '<a href="https://www.youtube.com/watch?v=WluFCmb6PmA">The Eras Tour</a>',
+            title: '<a href="https://grouvex.github.io/es/yt#TheErasTour">The Eras Tour</a>',
             description: 'The Eras Tour (<a href="https://www.taylorswift.com/">Taylor Swift</a><a>) - Grouvex Studio</a>'
         },
         // Añade más objetos de publicaciones de Instagram aquí

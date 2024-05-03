b088b8a6735af7351a9c7add0c1e42077fdf9fa6
Grouvex <168425996+Grouvex@users.noreply.github.com>
2024-05-03 23:05:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-  }
+}
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  }
+}
 
 function showNotification() {
-  document.getElementById("notification").style.display = "block";
+    document.getElementById("notification").style.display = "block";
 }
 window.onload = showNotification;
 
 function openVideo(url) {
-  document.getElementById('videoModal').style.display = 'block';
-  document.getElementById('videoFrame').src = url;
+    document.getElementById('videoModal').style.display = 'block';
+    document.getElementById('videoFrame').src = url;
 }
 function closeVideo() {
-  document.getElementById('videoModal').style.display = 'none';
-  document.getElementById('videoFrame').src = '';
+    document.getElementById('videoModal').style.display = 'none';
+    document.getElementById('videoFrame').src = '';
 }
 
 document.getElementById('themeSelect').addEventListener('change', function() {
-  // Obtiene los elementos body, main, article, section y h1
-  var elements = document.querySelectorAll('body, main,h1,h2,h3,h4,h5,h6,h7,h8,mainTop,.avatar1, article, section, aside');
-  // Elimina las clases de las opciones antiguas
-  elements.forEach(function(element) {
-    element.classList.remove('theme1', 'theme2', 'theme3', 'theme4', 'theme5');
-  });
-  // Si la opción seleccionada no es "default", añade la clase de la opción seleccionada
-  if (this.value !== 'default') {
+    // Obtiene los elementos body, main, article, section y h1
+    var elements = document.querySelectorAll('body, main,h1,h2,h3,h4,h5,h6,h7,h8,mainTop,.avatar1, article, section, aside');
+    // Elimina las clases de las opciones antiguas
     elements.forEach(function(element) {
-      element.classList.add(this.value);
+        element.classList.remove('theme1', 'theme2', 'theme3', 'theme4', 'theme5');
     });
-  }
+    // Si la opción seleccionada no es "default", añade la clase de la opción seleccionada
+    if (this.value !== 'default') {
+        elements.forEach(function(element) {
+            element.classList.add(this.value);
+        });
+    }
+    // Guarda la selección del usuario en el almacenamiento local
+    localStorage.setItem('selectedTheme', this.value);
+});
+
+window.addEventListener('load', function() {
+    // Obtiene la selección del usuario del almacenamiento local
+    var selectedTheme = localStorage.getItem('selectedTheme');
+    // Si hay una selección guardada y no es "default", aplica el tema seleccionado
+    if (selectedTheme && selectedTheme !== 'default') {
+        var elements = document.querySelectorAll('body, main,h1,h2,h3,h4,h5,h6,h7,h8,mainTop,.avatar1, article, section, aside');
+        elements.forEach(function(element) {
+            element.classList.add(selectedTheme);
+        });
+    }
 });

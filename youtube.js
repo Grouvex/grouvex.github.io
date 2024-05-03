1da7b6150064413132b4daa59d2c3ce374f5a151
Grouvex <168425996+Grouvex@users.noreply.github.com>
2024-05-03 14:55:48 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  // Primera Seccion
     var youtubeVideos = [
         {
             url: 'https://www.youtube.com/embed/x09D-9ZpF_o',
@@ -36,7 +37,26 @@ window.onload = function() {
                             '<iframe width="560" height="315" src="' + video.url + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         carousel.appendChild(videoDiv);
     });
+  // Segunda Seccion
+    var youtubeVideos = [
+        {
+            url: 'https://www.youtube.com/embed/WluFCmb6PmA',
+            title: '<a href="#TheErasTour">The Eras Tour</a>',
+            description: '<a>The Eras Tour (</a><a href="https://www.taylorswift.com/">Taylor Swift</a><a>) - Grouvex Studio</a>',
+            footer: '©2024 - Grouvex Studio'
+        },
+        // Añade más objetos de videos de YouTube aquí
+    ];
 
+    var carousel = document.getElementById('youtube-carousel1');
+
+    youtubeVideos.forEach(function(video) {
+        var videoDiv = document.createElement('div');
+        videoDiv.innerHTML = '<h2 class="post-title">' + video.title + '</h2>' +
+                            '<p class="post-description">' + video.description + '</p>' +
+                            '<iframe width="560" height="315" src="' + video.url + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        carousel.appendChild(videoDiv);
+    });
     // Código para el acordeón
     var acc = document.getElementsByClassName("accordion");
     var i;

d64b5ccba343a1142cde4107502252925b48759f
Grouvex <168425996+Grouvex@users.noreply.github.com>
2024-05-03 11:15:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function showNotification() {
   document.getElementById("notification").style.display = "block";
 }
 window.onload = showNotification;
+
+function openVideo(url) {
+  document.getElementById('videoModal').style.display = 'block';
+  document.getElementById('videoFrame').src = url;
+}
+function closeVideo() {
+  document.getElementById('videoModal').style.display = 'none';
+  document.getElementById('videoFrame').src = '';
+}

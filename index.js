36dc181d37e9d7d8d5a04e0b6fb6c421768558c3
Grouvex <168425996+Grouvex@users.noreply.github.com>
2024-05-18 19:24:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // Cuando se carga la página
-window.addEventListener('error', function(e) {
-    window.location.href = 'https://grouvex.github.io/404.html';
-}, true);
-
 window.addEventListener('load', function() {
     // Llama a todas las funciones que quieres ejecutar cuando se carga la página
     showNotification();openNav();closeNav();

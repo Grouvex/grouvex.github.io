496102d6e6a900fcaf5522911541a78e5f5c4b4e
Grouvex <168425996+Grouvex@users.noreply.github.com>
2024-05-25 15:43:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyDo0ReNmmfqLbZtfTLNQEFi9IwjOxzLrDI",
+    apiKey: "${{ secrets.apiKey }}",
     authDomain: "grouvex-a17b0.firebaseapp.com",
     projectId: "grouvex-a17b0",
     storageBucket: "grouvex-a17b0.appspot.com",

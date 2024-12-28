69bd90c14824417dd08c1398112b6ebad8cd8286
Grouvex <168425996+Grouvex@users.noreply.github.com>
2024-12-28 18:20:53 +0100

    Update a.js
    Signed-off-by: Grouvex <168425996+Grouvex@users.noreply.github.com>

--- a.js
+++ a.js
@@ -1,14 +1,1 @@
-// Cuando se carga la página
-window.addEventListener('load', function() {
-    flower();
-    sol();
-    leaf();
-    nieve();
-    aniversario();
-    googleTranslateElementInit();
-    setThemeImage('https://grouvex.com/img/Grouvex1.png');
-});
 
-function setThemeImage(imageUrl) {
-    document.querySelector('img-mainTop').src = imageUrl;
-}

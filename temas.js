a024151c338110c18e3061d0054582d78019b59d
Grouvex <168425996+Grouvex@users.noreply.github.com>
2025-03-05 21:01:23 +0100

    Update temas.js
    Signed-off-by: Grouvex <168425996+Grouvex@users.noreply.github.com>

--- temas.js
+++ temas.js
@@ -1,6 +1,6 @@
 (function() {
     const themeSelect = document.getElementById('themeSelect');
-    const defaultTheme = 'pokemon'; // Define aquí el tema predeterminado
+    const defaultTheme = 'gstudios5'; // Define aquí el tema predeterminado
     const currentUrl = window.location.href;
 
     // Deshabilitar el botón al inicio
@@ -34,6 +34,7 @@
             gstudios2: month !== 0,
             gstudios3: month !== 11,
             gstudios4: month !== 10,
+            gstudios4: month !== 9,
             starwars: !(date >= new Date(date.getFullYear(), 4, 4) && date <= new Date(date.getFullYear(), 4, 14)),
             httyd: !(date >= new Date(date.getFullYear(), 2, 25) && date <= new Date(date.getFullYear(), 3, 1)),
             jurassicworld: !(date >= new Date(date.getFullYear(), 5, 10) && date <= new Date(date.getFullYear(), 5, 20)),
@@ -60,9 +61,11 @@
 function changeTheme(theme) {
     const elements = document.querySelectorAll('p, body, main, mainTop, h1, h2, h3, h4, h5, h6, article, section, aside, panel, hr');
     const themeClasses = [
-        'theme1', 'theme2', 'space', 'starwars', 'jurassicworld', 'taylorswift', 'superheroes',
-        'naturaleza1', 'naturaleza2', 'naturaleza3', 'naturaleza4', 'pokemon', 'thewildrobot',
-        'httyd', 'gstudios1', 'gstudios2', 'gstudios3', 'gstudios4'
+        'theme1', 'theme2',
+        'space', 'naturaleza1', 'naturaleza2', 'naturaleza3', 'naturaleza4',
+        'taylorswift'
+        'pokemon', 'thewildrobot', 'httyd','starwars', 'jurassicworld', 'superheroes',
+        'gstudios1', 'gstudios2', 'gstudios3', 'gstudios4', 'gstudios5'
     ];
 
     // Aplicar la clase de animación
@@ -116,6 +119,7 @@ function changeTheme(theme) {
             gstudios2: month !== 0,
             gstudios3: month !== 11,
             gstudios4: month !== 10,
+            gstudios4: month !== 9,
             starwars: !(date >= new Date(date.getFullYear(), 4, 4) && date <= new Date(date.getFullYear(), 4, 14)),
             httyd: !(date >= new Date(date.getFullYear(), 2, 25) && date <= new Date(date.getFullYear(), 3, 1)),
             jurassicworld: !(date >= new Date(date.getFullYear(), 5, 10) && date <= new Date(date.getFullYear(), 5, 20)),
